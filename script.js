ce22a4ccfc9b656313968848e16a1a4d1c6f1a5a
Daniel Bronsky <dragonvedb@gmail.com>
2023-03-03 20:38:43 +0300

    Add working password confirmation by updating 'pattern' attribute on confirmation input to equal the password value.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const form = document.getElementById("sign-up");
+const password_input = document.getElementById("password");
+const password_confirm_input = document.getElementById("repeat-password");
 
-form.addEventListener('submit', validateForm);
+password_input.addEventListener('input', updateConfirmation);
+
+function updateConfirmation(e) {
+  password_confirm_input.setAttribute('pattern', `${e.target.value}`)
+}
+
+
+/*form.addEventListener('submit', validateForm);
 
 function validateForm(e) {
 
     const form = e.target;
   
     if (form.checkValidity()) {
-      console.log("OK!")
+        if (password_confirm_input.value !== password_input.value) {
+            password_confirm_input.classList.add('invalid');
+        }
     }
     else {
-      e.preventDefault();
-      console.log("NOT OK!")
-    }
-  };
+        e.preventDefault();
+        Array.from(form.elements).forEach(i => {
+        if (i.checkValidity()) {
+          // field is valid - remove class
+            i.parentElement.classList.remove('invalid');
+        }
+        else {
+          // field is invalid - add class
+            i.parentElement.classList.add('invalid');
+        }
+    });
+  };*/
