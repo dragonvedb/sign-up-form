41fc14d66eb1333268cfb71b92e9597aa05c4c5d
Daniel Bronsky <dragonvedb@gmail.com>
2023-03-03 21:37:49 +0300

    Create a custom error for when passwords are mismatched

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
 const form = document.getElementById("sign-up");
 const password_input = document.getElementById("password");
 const password_confirm_input = document.getElementById("repeat-password");
+const submit = document.getElementById("submit");
 
-password_input.addEventListener('input', updateConfirmation);
-
-function updateConfirmation(e) {
+password_input.addEventListener('input', e => {
   password_confirm_input.setAttribute('pattern', `${e.target.value}`)
-}
+});
 
-
-/*form.addEventListener('submit', validateForm);
-
-function validateForm(e) {
-
-    const form = e.target;
-  
-    if (form.checkValidity()) {
-        if (password_confirm_input.value !== password_input.value) {
-            password_confirm_input.classList.add('invalid');
-        }
-    }
-    else {
-        e.preventDefault();
-        Array.from(form.elements).forEach(i => {
-        if (i.checkValidity()) {
-          // field is valid - remove class
-            i.parentElement.classList.remove('invalid');
-        }
-        else {
-          // field is invalid - add class
-            i.parentElement.classList.add('invalid');
-        }
-    });
-  };*/
+submit.addEventListener('click',()=>{
+  if (password_confirm_input.validity.patternMismatch) {
+    password_confirm_input.setCustomValidity('Please make sure your passwords match');
+  } else { 
+    password_confirm_input.setCustomValidity('');
+  }
+})
